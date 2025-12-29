ce3844116d3b2ad5d148187c2dd56fa39bbdb777
webwisetarget <78576297+webwisetarget@users.noreply.github.com>
2025-12-29 14:59:34 +0700

    Comment out onBlur event handler in vue-multiselect

--- dist/vue-multiselect.esm.js
+++ dist/vue-multiselect.esm.js
@@ -1229,7 +1229,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     tabindex: _ctx.searchable ? -1 : $props.tabindex,
     class: normalizeClass([{ 'multiselect--active': _ctx.isOpen, 'multiselect--disabled': $props.disabled, 'multiselect--above': $options.isAbove, 'multiselect--has-options-group': $options.hasOptionGroup }, "multiselect"]),
     onFocus: _cache[14] || (_cache[14] = $event => (_ctx.activate())),
-    onBlur: _cache[15] || (_cache[15] = $event => (_ctx.searchable ? false : _ctx.deactivate())),
+    // onBlur: _cache[15] || (_cache[15] = $event => (_ctx.searchable ? false : _ctx.deactivate())),
     onKeydown: [
       _cache[16] || (_cache[16] = withKeys(withModifiers($event => (_ctx.pointerForward()), ["self","prevent"]), ["down"])),
       _cache[17] || (_cache[17] = withKeys(withModifiers($event => (_ctx.pointerBackward()), ["self","prevent"]), ["up"])),
